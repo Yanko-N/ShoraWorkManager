f140d1d8f71cbd220cfb77d76e03a60d112d5810
Vitor Novo <vitorjmnovo@gmail.com>
2025-10-27 14:24:18 +0000

    Created deafault values for Clients Listing Request

--- Application/Contracts/Request/ListingClientsRequest.cs
+++ Application/Contracts/Request/ListingClientsRequest.cs
@@ -5,9 +5,9 @@ namespace Application.Contracts.Request
 {
     public class ListingClientsRequest : IRequestable, ISearchable,IOrderable, ISortable<ClientSortBy>
     {
-        public int Page { get; set; }
-        public int PageSize { get; set; }
-        public string Search { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string Search { get; set; } = string.Empty;
         public ClientSortBy SortBy { get; set; } = ClientSortBy.None;
         public OrderByEnum OrderBy { get; set; } = OrderByEnum.Ascending;
 
